e3a6daf6bd73369d13b6e4c8623dec2b56cd5461
YHOSY <jsmarceloc@ceinmark.net>
2023-05-09 20:25:56 +0200

    new commit en una rama

--- PruebaMul/src/multiply/MultiplicatTbale.java
+++ PruebaMul/src/multiply/MultiplicatTbale.java
@@ -5,8 +5,10 @@ public class MultiplicatTbale {
 		multiply();
 	}
 	public static void multiply() {
+		//Modificaremos la tabla de multiplicar a 10 
 		for(int i=1; i<=10; i++) {
-			for (int j=0; j<=12; j++ ) {
+			for (int j=0; j<=10; j++ ) {
+				//Ahora se nos presentara la nueva tabla 
 				System.out.println(i + " * " + j + " = " + i*j);
 			}
 		}
