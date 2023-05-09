ab66c72cbc237cf42eb7f0ab56cbb0ed986e7d6f
YHOSY <jsmarceloc@ceinmark.net>
2023-05-09 20:17:55 +0200

    commit

---
+++ PruebaMul/src/multiply/MultiplicatTbale.java
@@ -0,0 +1,14 @@
+package multiply;
+
+public class MultiplicatTbale {
+	public static void main(String args[]) {
+		multiply();
+	}
+	public static void multiply() {
+		for(int i=1; i<=10; i++) {
+			for (int j=0; j<=12; j++ ) {
+				System.out.println(i + " * " + j + " = " + i*j);
+			}
+		}
+	}
+}
